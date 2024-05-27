0bd7681efde7bff43bd6ba8044e887449cddbbd2
Zoe Peng <zooey.peng@gmail.com>
2024-05-27 16:41:14 +0800

    feat: add question - snapshot

---
+++ Questions/Intermediate/19. snapshot.js
@@ -0,0 +1,7 @@
+// In the context of React, what is a "snapshot"?
+
+// A "snapshot" is a step in React's rendering flow that captures everything React needs to update the view at a specific moment in time. props, state, event handlers, and a description of the UI (based on those props and state) are all captured in the snapshot.
+
+// "snapshot" 是在特定時間點捕捉 React 需要更新視圖的所有信息的一個步驟。這包括了 props、state、事件處理程序，以及基於這些 props 和 state 描述的 UI 等。
+
+// "snapshot" 指的是在渲染過程中對組件的狀態和結構的快照。這個快照可以被用來進行性能優化、調試和測試等操作。它捕捉了組件在特定時間點的狀態和屬性，並提供了一個靜態的視圖，讓開發者可以查看和分析。總的來說，"snapshot" 在 React 中用於捕捉和記錄組件的狀態和結構，有助於開發者進行後續的操作和分析。
